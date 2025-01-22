31079b55a90e3d0f192b53944a4b9cd7a844e424
Xiaoyu Liu <liux@nbio.info>
2025-01-21 16:04:14 -0800

    Initial commit: HGVS extension package setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# HGVS Extension Package
+
+An extension package for HGVS variant description handling.
+
+## Installation
+
+```bash
+pip install hgvs-ext
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="hgvs-ext",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        # Add your project dependencies here
+    ],
+    python_requires=">=3.7",
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="HGVS Extension Package",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/hgvs-ext",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)

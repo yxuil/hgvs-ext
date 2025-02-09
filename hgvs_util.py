97187eede28705b092ec3b0bbecf84846e169fbf
Xiao-yu Liu <yxuil@users.noreply.github.com>
2025-02-08 21:14:03 -0800

    Create hgvs_util.py

---
+++ hgvs_util.py
@@ -0,0 +1,16 @@
+# %%
+import os
+from typing import Iterable
+from pathlib import Path
+import difflib
+import itertools
+from functools import cache
+
+from dotenv import load_dotenv
+
+local_env = Path('.env')
+home_env = Path('~/.env').expanduser()
+dotenv_file = local_env if local_env.exists() else home_env
+if dotenv_file.exists():
+  load_dotenv(dotenv_path=dotenv_file)
+

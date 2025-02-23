6e28d95cf96c69565569520b4cde687a1b3a63f3
Xiao-yu Liu <yxuil@users.noreply.github.com>
2025-02-23 00:05:53 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     install_requires=[
         # Add your project dependencies here
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.10",
     author="Your Name",
     author_email="your.email@example.com",
     description="HGVS Extension Package",
